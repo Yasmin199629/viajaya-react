7647cd3114e11537d87d3e28970e9abe8c4e73e2
Yasmin199629 <yasmiinartaza@gmail.com>
2025-07-08 15:52:03 -0300

    cambios

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,28 @@
-import React, { useState } from 'react';
+
+
+import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/navbar.css';
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
+   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
-
   return (
     <nav className="navbar">
-      <Link to="/" className="navbar-logo">
-        ViajaYa
-      </Link>
-
-    
-      
-        <li>
-          
-          <Link to="/" onClick={toggleMenu}>Inicio</Link>
-        </li>
-        <li>
-          <Link to="/destinos" onClick={toggleMenu}>Destinos</Link>
-        </li>
-        <li>
-          <Link to="/contacto" onClick={toggleMenu}>Contacto</Link>
-        </li>
-        <li>
-          <Link to="/nosotros" onClick={toggleMenu}>Nosotros</Link>
-        </li>
-      
-
-      <div className="navbar-toggle" onClick={toggleMenu}>
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
+      <div className="navbar-logo">
+        <Link to="/">ViajaYa</Link>
       </div>
+      <ul className="navbar-links">
+        <li><Link to="/">Inicio</Link></li>
+        <li><Link to="/destinos">Destinos</Link></li>
+        <li><Link to="/contacto">Contacto</Link></li>
+        <li><Link to="/nosotros">Nosotros</Link></li>
+      </ul>
     </nav>
   );
 }
 
 export default Navbar;
-
 
 
